0dc729d992437d564ac175db85abafbf581fb4ec
Faizan-Ahmad04 <faizanalvi093@gmail.com>
2025-01-21 00:36:18 +0530

    Design about and pricing component

---
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './Hero';
+import OpenAccount from '../OpenAccount';
+import Brokerage from './Brokerage';
+
+export default function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <OpenAccount />
+      <Brokerage />
+    </>
+  );
+}
